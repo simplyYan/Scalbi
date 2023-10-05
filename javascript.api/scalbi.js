6714002b13a3bac19eddc9fb4b5ea183a1ec49f4
Wesley Yan Soares Brehmer <102262759+simplyYan@users.noreply.github.com>
2023-10-05 19:20:52 +0000

    Add files via upload

---
+++ javascript.api/scalbi.js
@@ -0,0 +1,71 @@
+class Scalbi {
+  constructor(serverURL) {
+    this.serverURL = serverURL;
+    this.iframe = null;
+    this.targets = {};
+    this.pollHash();
+  }
+
+  create(id) {
+    if (!this.iframe) {
+      this.iframe = document.createElement('iframe');
+      this.iframe.id = id;
+      this.iframe.style.width = '100%';
+      this.iframe.style.height = '100%';
+      this.iframe.style.border = 'none';
+      document.body.appendChild(this.iframe);
+    }
+  }
+
+  set(targets) {
+    this.targets = targets;
+  }
+
+  send(method, data) {
+    if (!this.serverURL) {
+      console.error('URL do servidor não definido.');
+      return;
+    }
+
+    $.ajax({
+      type: method,
+      url: this.serverURL,
+      data: JSON.stringify(data),
+      contentType: 'application/json',
+      success: function (response) {
+        console.log('Resposta do servidor:', response);
+        // Faça o que quiser com a resposta do servidor aqui.
+      },
+      error: function (error) {
+        console.error('Erro na solicitação Ajax:', error);
+      },
+    });
+  }
+
+  pollHash() {
+    const self = this;
+    setInterval(function () {
+      const hash = window.location.hash.substring(1); // Remove o "#" do hash
+      const targetURL = self.targets[hash];
+      if (self.iframe && targetURL) {
+        self.iframe.src = targetURL;
+      }
+    }, 1400); // Verifica a cada 1,4 segundos
+  }
+}
+
+// Exemplo de uso:
+const sca = new Scalbi('http://localhost:8080/api/endpoint');
+
+sca.set({
+  home: 'index.html',
+  log: 'login.html',
+  dash: 'board.html',
+});
+
+var dados = {
+  campo: 'Valor de exemplo', // Seus dados aqui
+};
+
+sca.send('POST', dados);
+
